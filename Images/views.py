37d77739383117f49302262f890157ebcb25324b
Greyjacket <asch716@gmail.com>
2018-02-27 13:17:11 -0500

    Added upload form

--- Images/views.py
+++ Images/views.py
@@ -1,6 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, views
 from Images.models import Image
 from Images.serializers import ImageSerializer
+from rest_framework.decorators import parser_classes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import FileUploadParser
+from rest_framework.renderers import TemplateHTMLRenderer
 
 class image_list(generics.ListCreateAPIView):
     queryset = Image.objects.all()
@@ -9,3 +14,17 @@ class image_list(generics.ListCreateAPIView):
 class image_detail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+
+class image_upload(views.APIView):
+    parser_classes = (FileUploadParser,)
+    renderer_classes = (TemplateHTMLRenderer,)
+
+    def get(self,request):
+        return Response(template_name='upload_csv.html')
+
+    def put(self, request, filename, format=None):
+        file_obj = request.data['file']
+        # ...
+        # do some stuff with uploaded file
+        # ...
+        return Response(status=204)
