b292bd63a31133274ddef811887088dd2fe051ac
Greyjacket <asch716@gmail.com>
2018-03-01 14:19:02 -0500

    Parsing CSV

--- Images/views.py
+++ Images/views.py
@@ -7,6 +7,8 @@ from rest_framework.views import APIView
 from rest_framework.parsers import FileUploadParser
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.reverse import reverse
+import csv
+
 class image_list(generics.ListCreateAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
@@ -16,19 +18,45 @@ class image_detail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ImageSerializer
 
 class image_upload(views.APIView):
-    parser_classes = (FileUploadParser,)
+    #parser_classes = (FileUploadParser,)
     renderer_classes = (TemplateHTMLRenderer,)
 
     def get(self,request):
         return Response(template_name='upload_csv.html')
 
     def post(self, request):
-        #file_obj = request.data['file']
-        # ...
-        # do some stuff with uploaded file
-        # ...
-        return Response(template_name='failure_csv.html')
+        try:
+            csv_file = request.FILES['csv_file']
 
+            if not csv_file.name.endswith('.csv'):
+                return Response(template_name='failure_csv.html')
+
+            self.consume_csv(csv_file)
+
+            return Response(template_name='success_csv.html')
+        
+        except Exception as error:
+            print (error)
+            return Response(template_name='failure_csv.html')
+
+    def consume_csv(self, file):
+        file_data = file.read()
+        newCsv = []
+
+        with open(file_data, 'rb') as csvfile:
+            reader = csv.DictReader(csvfile)
+            row1 = next(reader)
+            #print(row1)
+            if self.validate_fields(row1):
+                for row in reader:
+                    newCsv.append(row)
+
+        #for item in newCsv:
+        #    print(item)  
+
+    def validate_fields(self, row):
+        #print (row)
+        return True
 '''
  data = {}
     if "GET" == request.method:
