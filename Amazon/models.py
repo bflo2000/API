003e6c150f26c562fe4b1b25866e7ebd9d309113
greyjacket <asch716@gmail.com>
2018-03-07 17:07:04 -0500

    Refined Image Fields

--- Amazon/models.py
+++ Amazon/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class BulletPoint(models.Model):
+	name = models.CharField(max_length=200, unique=True)
+    bullet = models.CharField(max_length=200, unique=True)
+
+class Variation(models.Model):
+	parent = models.ForeignKey()
+	catalog_number = models.CharField()
+	size_name = models.CharField()
+	variation_theme = models.CharField()
+	product_tax_code = models.CharField()
+	image_name = models.CharField()
+	bullet1 = models.CharField()
+	bullet2 = models.CharField()
+	bullet3 = models.CharField()
+	bullet4 = models.CharField()
+	bullet5 = models.CharField()
--- Images/models.py
+++ Images/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Image(models.Model):
     sku = models.CharField(max_length=16, unique=True)
+    old_sku = models.CharField(max_length=16, unique=True)
     name = models.CharField(max_length=128)
     height = models.IntegerField()
     width = models.IntegerField()
@@ -14,3 +15,7 @@ class Image(models.Model):
     sub_collection = models.CharField(max_length=72)
     url = models.CharField(max_length=200)
     path = models.CharField(max_length=400)
+    variations = models.CharField(max_length=400)
+    variation_group = models.CharField(max_length=20)
+    info = models.CharField() #
+    validated = models.Boolean()
