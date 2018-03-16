4b5d93095bd996e5a54d6e18d3578b92a484b261
Greyjacket <asch716@gmail.com>
2018-03-16 05:24:04 -0400

    Added DB schema drawing

--- Amazon/models.py
+++ Amazon/models.py
@@ -7,6 +7,7 @@ class BulletPoint(models.Model):
 
 class Amazon_Variation(models.Model):
 	parent_sku = models.OneToOneField(Image, on_delete=models.CASCADE)
+	item_sku = models.CharField(max_length = 200)
 	item_name = models.CharField(max_length = 200)
 	product_description = models.CharField(max_length = 200)
 	asin = models.CharField(max_length=64)
@@ -14,11 +15,12 @@ class Amazon_Variation(models.Model):
 	size_name = models.CharField(max_length=200, null=True, blank=True)
 	variation_theme = models.CharField(max_length=200)
 	product_tax_code = models.CharField(max_length=200)
-	parent = models.BooleanField()
+	is_parent = models.BooleanField()
 	bullet1 = models.CharField(max_length=200)
 	bullet2 = models.CharField(max_length=200)
 	bullet3 = models.CharField(max_length=200)
 	bullet4 = models.CharField(max_length=200)
 	bullet5 = models.CharField(max_length=200)
 	keywords = models.CharField(max_length=20000)
-	sales = models.DecimalField(max_digits = 10, decimal_places=2)
+	price = models.DecimalField(max_digits = 10, decimal_places=2, null=True, blank=True)
+	sales = models.IntegerField()
