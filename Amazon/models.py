7f6157b9142d1567713a36df25525a683191fbe1
Greyjacket <asch716@gmail.com>
2018-03-19 04:33:19 -0400

    Emergency Push

--- Amazon/models.py
+++ Amazon/models.py
@@ -6,7 +6,8 @@ class BulletPoint(models.Model):
 	bullet = models.CharField(max_length=200, unique=True)
 
 class Amazon_Variation(models.Model):
-	parent_sku = models.OneToOneField(Image, on_delete=models.CASCADE)
+	#parent_sku = models.OneToOneField(Image, on_delete=models.CASCADE)
+	parent_sku = models.ForeignKey(Image, on_delete=models.CASCADE)
 	item_sku = models.CharField(max_length = 200)
 	item_name = models.CharField(max_length = 200)
 	product_description = models.CharField(max_length = 200)
@@ -14,9 +15,9 @@ class Amazon_Variation(models.Model):
 	catalog_number = models.CharField(max_length=200, null=True, blank=True)
 	part_number = models.CharField(max_length=200, null=True, blank=True)
 	size_name = models.CharField(max_length=200, null=True, blank=True)
-	variation_theme = models.CharField(max_length=200)
-	product_tax_code = models.CharField(max_length=200)
-	currency = models.CharField(max_length=16)
+	variation_theme = models.CharField(max_length=200, null=True, blank=True)
+	product_tax_code = models.CharField(max_length=200, null=True, blank=True)
+	currency = models.CharField(max_length=16, null=True, blank=True)
 	is_parent = models.BooleanField()
 	bullet1 = models.CharField(max_length=200, null=True, blank=True)
 	bullet2 = models.CharField(max_length=200, null=True, blank=True)
@@ -25,7 +26,7 @@ class Amazon_Variation(models.Model):
 	bullet5 = models.CharField(max_length=200, null=True, blank=True)
 	keywords = models.CharField(max_length=20000, null=True, blank=True)
 	price = models.DecimalField(max_digits = 10, decimal_places=2, default=0)
-	sales = models.IntegerField()
+	sales = models.IntegerField(default=0)
 
 class Verification_Table(models.Model):
 	sku = models.OneToOneField(Amazon_Variation, on_delete=models.CASCADE)
