bebab0ac29f7ffa98e50ac6d06f83e1fbeab218e
EC2 Default User <ec2-user@ip-172-31-20-110.us-west-2.compute.internal>
2018-03-24 06:04:26 +0000

    Remote server changes

--- Amazon/models.py
+++ Amazon/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from Images.models import Image
+from decimal import Decimal
 
 class BulletPoint(models.Model):
 	name = models.CharField(max_length=200, unique=True)
 	bullet = models.CharField(max_length=200, unique=True)
 
 class Amazon_Variation(models.Model):
-	#parent_sku = models.OneToOneField(Image, on_delete=models.CASCADE)
-	parent_sku = models.ForeignKey(Image, on_delete=models.CASCADE)
-	item_sku = models.CharField(max_length = 200)
+	parent_sku = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+	item_sku = models.CharField(max_length = 200, unique=True)
 	item_name = models.CharField(max_length = 200)
-	product_description = models.CharField(max_length = 200)
+	product_description = models.CharField(max_length = 2000)
 	asin = models.CharField(max_length=64, null=True, blank=True)
 	catalog_number = models.CharField(max_length=200, null=True, blank=True)
 	part_number = models.CharField(max_length=200, null=True, blank=True)
@@ -19,12 +19,15 @@ class Amazon_Variation(models.Model):
 	product_tax_code = models.CharField(max_length=200, null=True, blank=True)
 	currency = models.CharField(max_length=16, null=True, blank=True)
 	is_parent = models.BooleanField()
+	is_orphan = models.BooleanField(default=True)
+	check_bullets = models.BooleanField(default=False)
+	check_unicode = models.BooleanField(default=False)
 	bullet1 = models.CharField(max_length=200, null=True, blank=True)
 	bullet2 = models.CharField(max_length=200, null=True, blank=True)
 	bullet3 = models.CharField(max_length=200, null=True, blank=True)
 	bullet4 = models.CharField(max_length=200, null=True, blank=True)
-	bullet5 = models.CharField(max_length=200, null=True, blank=True)
-	keywords = models.CharField(max_length=20000, null=True, blank=True)
+	bullet5 = models.CharField(max_length=2000, null=True, blank=True)
+	keywords = models.CharField(max_length=4000, null=True, blank=True)
 	price = models.DecimalField(max_digits = 10, decimal_places=2, default=0)
 	sales = models.IntegerField(default=0)
 
--- Amazon/views.py
+++ Amazon/views.py
@@ -9,6 +9,10 @@ from rest_framework.parsers import FileUploadParser
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.reverse import reverse
 import csv
+import logging
+from decimal import Decimal
+
+logger = logging.getLogger(__name__)
 
 class amazon_variation_list(generics.ListCreateAPIView):
 	queryset = Amazon_Variation.objects.all()
@@ -32,7 +36,8 @@ class amazon_variation_upload(views.APIView):
 				print ('File is not a CSV.')
 				return Response(template_name='failure_csv_amazon.html')
 
-			file_data = csv_file.read().decode("utf-8")  
+			#file_data = csv_file.read().decode("utf-8")
+			file_data = csv_file.read()
 			lines = file_data.split("\n")
 			reader = csv.DictReader(lines)
 
@@ -43,59 +48,113 @@ class amazon_variation_upload(views.APIView):
 				return Response(template_name='failure_csv_amazon.html')
 
 			return Response(template_name='success_csv_amazon.html')
-		
+
 		except Exception as error:
 			print (error)
 			return Response(template_name='failure_csv_amazon.html')
 
-	def consume_csv(self, reader):        
+	def consume_csv(self, reader):
+		#try:
 
 		for row in reader:
 			data = {}
 
+			if row['parent_child'] == 'parent':
+				data['is_parent'] = True
+				parent_sku = row['item_sku']
+			else:
+				data['is_parent'] = False
+				parent_sku = row['parent_sku']
+
+			# if parent does not exist, ignore
 			try:
-				row['parent_sku'] = '101579P'
-				image = Image.objects.get(sku=row['parent_sku'])
-				data['parent_sku'] = image.id
-				data['item_sku'] = row['item_sku']
-				data['item_name'] = row['item_name']
-				data['product_description'] = row['product_description']
-				data['asin'] = row['external_product_id']
-				data['catalog_number'] = row['catalog_number']
-				data['part_number'] = row['part_number']
-				data['size_name'] = row['size_name']
-				data['variation_theme'] = row['variation_theme']
-				data['product_tax_code'] = row['product_tax_code']
-				data['currency'] = row['currency']
-				
-				if row['parent_child'] == 'parent':
-					data['is_parent'] = True
-				else:
-					data['is_parent'] = False
+				image = Image.objects.get(sku=parent_sku)
+				data['is_orphan'] = False
+				data['parent_sku'] = image.sku
 
-				data['bullet1'] = row['bullet_point1']
-				data['bullet2'] = row['bullet_point2']
-				data['bullet3'] = row['bullet_point3']
-				data['bullet4'] = row['bullet_point4']
-				data['bullet5'] = row['bullet_point5']
+			except:
+				data['is_orphan'] = True
 
-				data['keywords'] = row['generic_keywords1']
-				data['price'] = (row['standard_price'])
+			data['item_sku'] = row['item_sku']
 
-			except Exception as error:
-				print(error)
-				return False
+			# retrieve the product description - and check unicode
+			data['item_name'] = row['item_name']
+
+			try:
+				test = (unicode(data['item_name']))
+			except:
+				print('item_name unicode', row['item_name'], row['item_sku'])
+				data['item_name'] = row['item_name'].decode('utf-8')
+				data['check_unicode'] = True
+
+			# retrieve the product description - and check unicode
+			data['product_description'] = row['product_description']
+
+			try:
+				test = (unicode(data['product_description']))
+			except:
+				print('product description unicode')
+				data['product_description'] = row['product_description'].decode('utf-8')
+				data['check_unicode'] = True
+
+			# retrieve the fifth bullet, as it may be a copy of the title - and check unicode
+			bullet5 = row['bullet_point5']
+			if len(bullet5) > 200:
+				bullet5 = bullet5[:200]
+				data['check_bullets'] = True
+
+			try:
+				test = (unicode(bullet5))
+			except:
+				print('bullet5 unicode')
+				data['bullet5'] = bullet5.decode('utf-8')
+				data['check_unicode'] = True
+
+			# retrieve keywords, check unicode
+			data['keywords'] = row['generic_keywords1']
+
+			try:
+				test = (unicode(data['keywords']))
+			except:
+				print('keywords unicode')
+				data['keywords'] = row['generic_keywords1'].decode('utf-8')
+				data['check_unicode'] = True
+
+			data['asin'] = row['external_product_id']
+			data['catalog_number'] = row['catalog_number']
+			data['part_number'] = row['part_number']
+			data['size_name'] = row['size_name']
+			data['variation_theme'] = row['variation_theme']
+			data['product_tax_code'] = row['product_tax_code']
+			data['currency'] = row['currency']
+			data['bullet1'] = row['bullet_point1']
+			data['bullet2'] = row['bullet_point2']
+			data['bullet3'] = row['bullet_point3']
+			data['bullet4'] = row['bullet_point4']
+
+			# set default price to 0.00 if empty
+			if row['standard_price'] == "":
+				data['price'] = '0.00'
+			else:
+				data['price'] = row['standard_price']
 
 			try:
 				serializer = Amazon_Variation_Serializer(data=data)
 				if serializer.is_valid():
 					serializer.save()
 				else:
-					print (serializer.errors)
-					return False
-					
-			except Exception as error:
-				print(error)
+					print ('Amazon Serializer:', serializer.errors)
+					print (data['item_sku'])
+					#return False
+					#pass
+
+			except UnicodeDecodeError as error:
+				print('Exception', error)
+				print (row['item_sku'])
 				return False
+
+		#except Exception as error:
+		#	print('Error:', error)
+			#return False
 
 		return True
